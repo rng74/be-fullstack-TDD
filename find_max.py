2116ea93d2c479cea0cd9f525b7ba49dcae692a5
Yersultan Nadyrmagambet <nadyrcoop@gmail.com>
2018-10-12 00:01:34 +0600

    Assignment finish

--- find_max.py
+++ find_max.py
@@ -1,29 +1,35 @@
 def get_max(a, b):
-    pass
+    return a if a > b else b
 
 
 def get_max_without_arguments():
-    pass
+    raise "No arguments"
 
 
 def get_max_with_one_argument(a):
-    pass
+    return a
 
 
 def get_max_with_many_arguments(*args):
-    pass
+    maxx = -float('inf')
+    for i in args:
+        maxx = i if (i > maxx) else maxx
+    return maxx
 
 
 def get_max_with_one_or_more_arguments(first, *args):
-    pass
+    return first if first > get_max_with_many_arguments(*args) else get_max_with_many_arguments(*args)
 
 
 def get_max_bounded(*args, low, high):
-    pass
+    maxx = -float('inf')
+    for i in args:
+        if i >= low and i <= high:
+            maxx = i if i > maxx else maxx;
+    return maxx
 
 
 def make_max(*, low, high):
     def inner(first, *args):
-        pass
-
+        return get_max_bounded(first, *args, low = low, high = high)
     return inner
--- find_min.py
+++ find_min.py
@@ -1,50 +1,29 @@
-
-
 def get_min(a, b):
-    """
-        return min number among a and b
-    """
-
+    return a if a < b else b
 
 def get_min_without_arguments():
-    """
-        raise TypeError exception with message
-    """
-
+    raise "No arguments"
 
 def get_min_with_one_argument(x):
-    """
-        return that value
-    """
-
+    return x
 
 def get_min_with_many_arguments(*args):
-    """
-        return smallest number among args
-    """
-
+    mini = float('inf')
+    for i in args:
+        mini = i if (i < mini) else mini
+    return mini
 
 def get_min_with_one_or_more_arguments(first, *args):
-    """
-        return smallest number among first + args
-    """
-
+    return first if first < get_min_with_many_arguments(*args) else get_min_with_many_arguments(*args)
 
 def get_min_bounded(*args, low, high):
-    """
-        return smallest number among args bounded by low & high
-    """
-
+    mini = float('inf')
+    for i in args:
+        if i >= low and i <= high:
+            mini = i if i < mini else mini;
+    return mini
 
 def make_min(*, low, high):
-    """
-        return inner function object which takes at last one argument
-        and return smallest number amount it's arguments, but if the
-        smallest number is lower than the 'low' which given as required
-        argument the inner function has to return it.
-    """
-
     def inner(first, *args):
-        pass
-
+        return get_min_bounded(first, *args, low = low, high = high)
     return inner
